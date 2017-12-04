7083a1bf86873b0b66f0b14c39f3cb9600fcc295
khapeterk <khapeterk@gmail.com>
2017-12-04 18:15:21 -0500

    wrote first expect for test function

--- spec/the-time-in-words.spec.js
+++ spec/the-time-in-words.spec.js
@@ -1,5 +1,5 @@
 describe('a function for the time in words', () => {
   it('should translate 5:00 to five o\' clock', () => {
-    
+    expect(theTimeInWords('5:00')).toBe('five o\' clock')
   })
 })
