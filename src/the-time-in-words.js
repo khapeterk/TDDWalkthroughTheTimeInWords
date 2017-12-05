31600f11a63d428424f54c77b8e5f070a2086705
khapeterk <khapeterk@gmail.com>
2017-12-04 23:03:04 -0500

    refactored minutes and moved return down

--- src/the-time-in-words.js
+++ src/the-time-in-words.js
@@ -1,15 +1,16 @@
 function theTimeInWords(time) {
   let hour = 'five'
   let pastString = ' past '
+  let minutes
   if (time === '5:00') {
     return hour + ' o\' clock'    
   }
   if (time === '5:15') {
-    let minutes = 'quarter'
-    return minutes + pastString + hour
+    minutes = 'quarter'
   }
   if (time === '5:30') {
-    let minutes = 'half'
-    return minutes + pastString + hour    
+    minutes = 'half'
   }
+  return minutes + pastString + hour    
+  
 }
