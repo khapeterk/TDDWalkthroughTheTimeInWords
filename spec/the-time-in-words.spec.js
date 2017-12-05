c6fa1eb78fd0e61915f95ad0baf99abf9686feea
khapeterk <khapeterk@gmail.com>
2017-12-04 20:58:20 -0500

    added test for half past five

--- spec/the-time-in-words.spec.js
+++ spec/the-time-in-words.spec.js
@@ -2,4 +2,7 @@ describe('a function for the time in words', () => {
   it('should translate 5:00 to five o\' clock', () => {
     expect(theTimeInWords('5:00')).toBe('five o\' clock')
   })
+  it('should translate 5:30 to half past five', () => {
+    expect(theTimeInWords('5:30')).toBe('half past five')
+  })
 })
