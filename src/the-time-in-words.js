b9b6620471d952f89f92917d33fe2d1310f25c09
khapeterk <khapeterk@gmail.com>
2017-12-04 22:12:11 -0500

    implemented quarter past five

--- src/the-time-in-words.js
+++ src/the-time-in-words.js
@@ -1,5 +1,8 @@
 function theTimeInWords(time) {
   let hour = 'five'
+  if (time === '5:15') {
+    return 'quarter past ' + hour
+  }
   if (time === '5:30') {
     return 'half past ' + hour    
   }
