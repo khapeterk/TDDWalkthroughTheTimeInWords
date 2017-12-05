edd9147d1e947e027faedb5e8b710b3287a05c0d
khapeterk <khapeterk@gmail.com>
2017-12-04 22:36:03 -0500

    moved hour o clock if block up

--- src/the-time-in-words.js
+++ src/the-time-in-words.js
@@ -1,6 +1,9 @@
 function theTimeInWords(time) {
   let hour = 'five'
   let pastString = ' past '
+  if (time === '5:00') {
+    return hour + ' o\' clock'    
+  }
   if (time === '5:15') {
     let minutes = 'quarter'
     return minutes + pastString + hour
@@ -8,8 +11,5 @@ function theTimeInWords(time) {
   if (time === '5:30') {
     let minutes = 'half'
     return minutes + pastString + hour    
-  }
-  if (time === '5:00') {
-    return hour + ' o\' clock'    
   }
 }
